ebae0781698df36b05a3748a88d32fb57154137b
tianfeng1598 <45988639+tianfeng1598@users.noreply.github.com>
2019-07-03 00:30:09 +0800

    Add files via upload
    更新文件的批处理方法，可以通过指定的文件目录，进行扫描、检索、输出。

--- src/wordcount/WordCount.java
+++ src/wordcount/WordCount.java
@@ -1,5 +1,4 @@
-package wordcount;
-
+package test;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -12,29 +11,62 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class WordCount {
+	private static StringBuffer temp = new StringBuffer();
+	private static int Count;
 
 	public static void main(String[] args) {
-		File keyfile = new File("D:/J.txt");
-		File file = new File("D:/1999.txt");
-		Map<String, Integer> result = wordutil(keyfile, file);
-		creatResult("D:/result.txt", result);
+
+		// 获得文件的绝对路径
+		// 需要检索的文件总目录 "E:/test/" 这个目录下的所有文件都会被检索，无所谓多少个文件夹
+		List<String> fileList = findFiles("E:/test/");
+		System.out.println(fileList.toString());
+		
+		// 进行检索
+		// 关键词的位置是写死的。
+		String keyfile = "E:/xjxj.txt";
+		for (String file : fileList) {
+			Word(keyfile,file);
+		}
 	}
+	/**
+	 * 這個用於輸出每個文件的匹配結果文件
+	 * @param keyfilePath
+	 * @param filePath
+	 */
+	public static void Word(String keyfilePath,String filePath) {
+		// TODO Auto-generated constructor stub
+		Count = 0;
+		temp = new StringBuffer();
+		//匹配的词库文件
+		File keyfile = new File(keyfilePath);
+		//处理的词库文件
+		File file = new File(filePath);
+		Map<String, Integer> result = wordutil(keyfile, file);
+		//生成的结果文件
+		creatResult(filePath.replace(".", "result."), result);
+		//生成汇总文件
+		fileCount("E:/test/tj.txt",file.getName());
+		
+		System.out.println(result);
+	}
+	
+	
 
 	// 读取目标文件中的关键字,返回一个list<String>
 	private static List<String> KeyWords(File file) throws IOException {
 		System.out.println("读取关键字文件......");
 		List<String> list = new LinkedList<>(); // 用于接收关键字的容器
-		Scanner scan1 = new Scanner(file,"GBK"); // 建立一个读取对象
+		Scanner scan1 = new Scanner(file,"utf-8"); // 建立一个读取对象
 		while (true) {
 			if (scan1.hasNext() == false)
 				break; // hasNext() 判断是否有后文
 			String s = scan1.nextLine(); // 读取下一行
-			//System.out.println(s);
 			list.add(s.trim());
 		}
 		scan1.close();
 		System.out.println("读取关键字文件结束!");
 		return list;
+		
 	}
 
 	// 对目标文件进行检索,并输出int值
@@ -45,13 +77,14 @@ public class WordCount {
 			if (scan.hasNext() == false)
 				break; // hasNext() 判断是否有后文
 			String s = scan.nextLine(); // 读取下一行
-			//System.out.println(s);
 			if (s.contains(keyword)) { // 判断是否存在
 				k++;
-				if(k > 0){
-					System.out.println(keyword);
-				}
+				System.out.println(s);
+				temp.append(keyword+" ");
+				System.out.println(keyword);
+				System.out.println("查到了!");
 			}
+
 		}
 		scan.close();
 		return k;
@@ -67,8 +100,12 @@ public class WordCount {
 			for (String keyword : KeyWords) {
 				int counts = 0;
 				counts = Wordcount(file, keyword);
-				results.put(keyword, counts); // 写入结果集
+				if(counts!=0){
+					Count += counts;  // 统计最终的计数结果
+					results.put(keyword, counts); // 写入结果集	
+				}				
 			}
+			System.out.println(KeyWords);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -81,6 +118,9 @@ public class WordCount {
 	private static void creatResult(String path, Map<String, Integer> result) {
 		System.out.println("输出结果中.......");
 		File file = new File(path); // 建立一个路径
+
+		LCWJ(path,file);
+		
 		OutputStream os = null;
 		BufferedOutputStream bos = null;
 		try {
@@ -89,10 +129,10 @@ public class WordCount {
 			}
 			os = new FileOutputStream(file);
 			bos = new BufferedOutputStream(os);
-			for (String keyword : result.keySet()) {
-				String word = keyword + " " + result.get(keyword)+"\r\n";
-				byte[] bt = word.getBytes("utf-8");
-				bos.write(bt);
+			for (String keyword : result.keySet()) {			
+					String word = keyword + " " + result.get(keyword)+"\r\n";
+					byte[] bt = word.getBytes("utf-8");
+					bos.write(bt);			
 			}
 			bos.flush();
 		} catch (IOException e) {
@@ -113,4 +153,90 @@ public class WordCount {
 		}
 		System.out.println("结果输出完成!");
 	}
+	
+	// 打印流程文件
+	private static void LCWJ(String path,File file){
+		System.out.println(path);		
+		File files = new File(path.replace(".", "-R."));
+		OutputStream os = null;
+		BufferedOutputStream bos = null;
+		System.out.println(temp.toString());
+		try {
+			if (!files.exists()) {
+				files.createNewFile();
+			}
+			os = new FileOutputStream(files);
+			bos = new BufferedOutputStream(os);
+			System.out.println("+++++++++++++++++++++++++++++++++++++++");
+			bos.write(temp.toString().getBytes("utf-8"));	
+			bos.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bos != null) {
+					bos.close();
+				}
+				if (os != null) {
+					os.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	//生成的结果文件
+	private static void fileCount(String path,String name){
+		File file = new File(path); // 建立一个路径
+		String word = name+"："+Count+"\r\n";
+		OutputStream os = null;
+		BufferedOutputStream bos = null;
+		try {
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			os = new FileOutputStream(file,true);
+			bos = new BufferedOutputStream(os);
+			bos.write(word.getBytes("utf-8"));
+			bos.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bos != null) {
+					bos.close();
+				}
+				if (os != null) {
+					os.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	//用于读取目录下的文件
+	private static List<String> findFiles(String path){
+		List<String> list = new LinkedList<String>();
+		File f = new File(path);
+		if(f != null){
+			File[] fs = f.listFiles();
+			for (File fil : fs) {
+				if(fil.isFile()){
+					System.out.println(fil.getAbsolutePath());
+					list.add(fil.getAbsolutePath());
+				}else{
+					findFiles(fil.getAbsolutePath());
+				}
+			}
+		}
+		return list;
+	}
+	
 }
